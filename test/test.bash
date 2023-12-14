a8ea19ce3d27b184be3a13f97ca70fe8c76cd7ed
Nozaki <sorakara656@gmail.com>
2023-12-15 06:07:46 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -1,12 +1,33 @@
 #!/bin/bash
+#SPDX-License-Identifier: BSD-3-Clause
 
-dir=~
-[ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
+# 変数定義
+DIR=~/ros2_ws
+PKG_NAME=pi_calculator_pkg # ROS2パッケージ名を変更してください
+LAUNCH_FILE=pi_calculator.launch.py # プログラム専用の適切なローンチファイル名に変更してください
+LOG_FILE=/tmp/${PKG_NAME}_test.log
+TIMEOUT_DURATION=15 # 期待する出力をキャッチするために必要な時間
 
-cd $dir/ros2_ws
-colcon build
-source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+# 引数がある場合、指定されたディレクトリを使用
+[ "$1" != "" ] && DIR="$1"
 
-cat /tmp/mypkg.log |
-grep 'Listen: 8'
+# ROS2 ワークスペースの準備
+source /opt/ros/foxy/setup.bash # ROS2環境を準備するために適切なROS2バージョンをsourceしてください
+cd $DIR
+colcon build --packages-select $PKG_NAME
+
+# 環境をsourceする
+source $DIR/install/setup.bash
+
+# ローンチファイルを実行しログに出力、その後出力をチェック
+timeout $TIMEOUT_DURATION ros2 launch $PKG_NAME $LAUNCH_FILE > $LOG_FILE
+
+# 出力値の確認 - ここでPiの近似として3.14を使っていますが、必要に応じて調整してください
+grep "data: 3.14" $LOG_FILE
+if [ $? -eq 0 ]; then
+    echo "Test passed: Pi estimate found in log."
+    exit 0
+else
+    echo "Test failed: Pi estimate not found in log."
+    exit 1
+fi
