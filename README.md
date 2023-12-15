659f2557c7504c871230668e5e617874012113c5
Nozaki <sorakara656@gmail.com>
2023-12-15 10:01:51 +0900

    1.3

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 # mypkg
 
 
-ロボットシステム学のros2練習
+ロボットシステム学で使用したros2
 
 # リポジトリ内のノード,ファイル一覧
 
-### talker.py
-* パブリッシャを持つノード. モンテカルロ法を利用してπ（3．14）の近似値を求めて最終的にπ（3．14）に近くなる過程トピック`/pi_setimate`を通じて送信する.
+## talker.py
+* パブリッシャのノード.  
+* モンテカルロ法を利用してπ（3．14）の近似値を求めて最終的にπ（3．14）に近くなる過程トピック`/pi_setimate`を通じて送信する.
 
-### listener.py
-* サブスクライバを持つノード. トピック`/pi_setimate`からメッセージを受け取り表示する.
+## listener.py
+* サブスクライバのノード.  
+* トピック`/pi_setimate`からメッセージを受け取り表示する.
 
-## 実行手順
-### talkerとlistener
+# 実行手順
+## talkerとlistener
 * `ros2 run`で実行する方法
-bash
 端末1$ ros2 run mypkg talker
 端末2$ ros2 run mypkg listener
--- test/test.bash
+++ test/test.bash
@@ -1,28 +1,22 @@
 #!/bin/bash
 #SPDX-License-Identifier: BSD-3-Clause
 
-# 変数定義
 DIR=~/ros2_ws
-PKG_NAME=mypkg # ROS2パッケージ名を変更してください
-LAUNCH_FILE=talk_listen.launch.py # プログラム専用の適切なローンチファイル名に変更してください
+PKG_NAME=mypkg
+LAUNCH_FILE=talk_listen.launch.py 
 LOG_FILE=/tmp/${PKG_NAME}_test.log
-TIMEOUT_DURATION=15 # 期待する出力をキャッチするために必要な時間
+TIMEOUT_DURATION=20
 
-# 引数がある場合、指定されたディレクトリを使用
 [ "$1" != "" ] && DIR="$1"
 
-# ROS2 ワークスペースの準備
-source /opt/ros/foxy/setup.bash # ROS2環境を準備するために適切なROS2バージョンをsourceしてください
+source /opt/ros/foxy/setup.bash
 cd $DIR
 colcon build --packages-select $PKG_NAME
 
-# 環境をsourceする
 source $DIR/install/setup.bash
 
-# ローンチファイルを実行しログに出力、その後出力をチェック
-pwd
-ls
-timeout $TIMEOUT_DURATION ros2 launch $PKG_NAME $LAUNCH_FILE > $LOG_FILE
+
+timeout $TIMEOUT_DURATION "ros2 launch $PKG_NAME $LAUNCH_FILE" > $LOG_FILE
 
 # 出力値の確認 - ここでPiの近似として3.14を使っていますが、必要に応じて調整してください
 grep "data: 3.14" $LOG_FILE
