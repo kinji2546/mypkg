c231280a496b78ce13bc239cb88d300f711d1792
Nozaki <sorakara656@gmail.com>
2023-12-16 23:25:01 +0900

    echosin

--- test/test.bash
+++ test/test.bash
@@ -16,17 +16,17 @@ colcon build --packages-select $PKG_NAME
 source $DIR/ros2_ws/install/setup.bash
 
 ros2 launch $PKG_NAME $LAUNCH_FILE > $LOG_FILE 2>&1 &
-echo $?
+if [ -e $LOG_FILE ]; then
+  echo "Log Fileあるよ."
+fi
 sleep $TIMEOUT_DURATION
 pkill -f 'ros2 launch'
 
 grep "pi estimate:" $LOG_FILE | grep "3.14"
-if [ -e $LOG_FILE ]; then
-  echo "Log Fileあるよ."
+if [ $? -eq 0 ]; then
+    echo "Test passed: Pi estimate found in log."
+    exit 0
+else
+    echo "Test failed: Pi estimate not found in log."
+    exit 1
 fi
-  #  echo "Test passed: Pi estimate found in log."
-   # exit 0
-#else
- #   echo "Test failed: Pi estimate not found in log."
-  #  exit 1
-#fi
