e8fae5ac3023f6e855fa3d7fa8de57a0291db5b1
Nozaki <sorakara656@gmail.com>
2023-12-16 23:43:46 +0900

    re:

--- test/test.bash
+++ test/test.bash
@@ -8,20 +8,26 @@ LOG_FILE=/tmp/${PKG_NAME}_test.log
 TIMEOUT_DURATION=20
 
 [ "$1" != "" ] && DIR="$1"
-
+# ROS 2 foxyのセットアップ
 # source /opt/ros/foxy/setup.bash
+# ワークスペースに移動してビルド
 cd $DIR/ros2_ws
 colcon build --packages-select $PKG_NAME
 
+# ビルドしたパッケージのセットアップ
 source $DIR/ros2_ws/install/setup.bash
 
+# テスト用ランチファイルの実行とログの出力
 ros2 launch $PKG_NAME $LAUNCH_FILE > $LOG_FILE 2>&1 &
 
+# ランチファイルが時間内に完了するのを待機
 sleep $TIMEOUT_DURATION
 pkill -f 'ros2 launch'
 if [ -e $LOG_FILE ]; then
   echo "Log Fileあるよ."
 fi
+
+# 出力値の確認 - ここでPiの近似値として3.14
 grep "pi estimate:" $LOG_FILE | grep "3.14"
 if [ $? -eq 0 ]; then
     echo "Test passed: Pi estimate found in log."
