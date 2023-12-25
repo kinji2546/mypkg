cf7808751c5dad9351de170c89460422373ac3b2
Nozaki <sorakara656@gmail.com>
2023-12-26 01:03:14 +0900

    &&add

--- test/test.bash
+++ test/test.bash
@@ -15,6 +15,5 @@ source "$dir/ros2_ws/install/setup.bash"
 # ノードを10秒間ランチしてログに出力、その後ログを解析する
 timeout 560 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 # ログファイルを解析し、「3.141」が含まれている場合は 0 を返す
-grep '3.1415' /tmp/mypkg.log
- echo "Found '3.1415' in the log."
+grep -q '3.1415' /tmp/mypkg.log && echo "Found '3.1415' in the log." || echo "Did not find '3.1415' in the log."
   
