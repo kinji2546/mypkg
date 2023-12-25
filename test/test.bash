449cc823b8cfac4afa9aa14680290a045bc6dbd9
Nozaki <sorakara656@gmail.com>
2023-12-25 21:01:28 +0900

    0&1add

--- test/test.bash
+++ test/test.bash
@@ -14,4 +14,11 @@ source "$dir/ros2_ws/install/setup.bash"
 
 # ノードを10秒間ランチしてログに出力、その後ログを解析する
 timeout 20 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-grep 'Pi approximate value:' /tmp/mypkg.log | tail -n 1
+# ログファイルを解析し、「3.141」が含まれている場合は 0 を返す
+if grep -q '3.141' /tmp/mypkg.log; then
+  echo "Found '3.141' in the log."
+  exit 0
+else
+  echo "Did not find '3.141' in the log."
+  exit 1
+fi
